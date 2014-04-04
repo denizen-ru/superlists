35d6f7b935916e4ebaf89993719e3cbe355fcc03
Denis Knyazev <denis.m.knyazev@gmail.com>
2014-04-04 17:57:17 +0400

    Before refactorinf.

--- functional_tests.py
+++ functional_tests.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 
 class NewVisitorTest(unittest.TestCase):
 
-    """docstring for NewVisitorTest"""
+    """tests for home page"""
 
     def setUp(self):
         self.browser = webdriver.Chrome()
@@ -21,14 +22,14 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', header_text)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        self.assertEqual(inputbox.get_attribute(
+            'placeholder'), 'Enter a to-do item')
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = self.browser.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
-                        "New to-do item did not appear in table")
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
