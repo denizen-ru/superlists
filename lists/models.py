dba71ac6ad673366c17c94a506d16393d39ea5db
Denis Knyazev <denis.m.knyazev@gmail.com>
2014-04-06 22:16:55 +0400

    Add new model List and changed tests for it.

--- lists/models.py
+++ lists/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 
+class List(models.Model):
+    pass
+
+
 class Item(models.Model):
 
     """docstring for Item"""
     text = models.TextField()
+    list = models.ForeignKey(List)
