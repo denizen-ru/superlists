d5cd4857b513da7baab03c9368c04bedfdf6a418
Denis Knyazev <denis.m.knyazev@gmail.com>
2014-04-07 09:54:58 +0400

    First step of FT for layout & styling.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,6 +1,7 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import unittest
 import time
 
 
@@ -20,6 +21,7 @@ class NewVisitorTest(LiveServerTestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
+    @unittest.skip('long test')
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url)
         self.assertIn('To-Do', self.browser.title)
@@ -62,3 +64,14 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
+
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        # inputbox.send_keys('testing\n')
+        print inputbox.location
+        x = inputbox.location['x']
+        width = inputbox.size['width']
+        self.assertAlmostEqual(x + width / 2, 512, delta=5)
