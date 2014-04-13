d0c9d7a2a62cf4ba3fd413452e80641ee0f1b430
Denis Knyazev <denis.m.knyazev@gmail.com>
2014-04-13 05:24:09 +0400

    Moved FTs into their own individual files.

---
+++ functional_tests/test_layout_and_styling.py
@@ -0,0 +1,15 @@
+from .base import FunctionalTest
+
+
+class LayoutAndStylingTest(FunctionalTest):
+
+    def test_layout_and_styling(self):
+        self.browser.get(self.server_url)
+        self.browser.set_window_size(1024, 768)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        # inputbox.send_keys('testing')
+        # inputbox.send_keys(Keys.ENTER)
+        x = inputbox.location['x']
+        width = inputbox.size['width']
+        self.assertAlmostEqual(x + width / 2, 512, delta=6)
