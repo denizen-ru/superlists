c9957011c8ff1cd397469312cc7a9eb4c3c98493
Denis Knyazev <denis.m.knyazev@gmail.com>
2014-04-17 01:15:17 +0400

    Use new form in home_page, simplify tests. NB breaks stuff.

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -7,21 +7,21 @@ from unittest import skip
 
 from lists.views import home_page
 from lists.models import Item, List
+from lists.forms import ItemForm
 
 
 class HomePageTest(TestCase):
 
     """docstring for HomePageTest"""
+    maxDiff = None
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    def test_home_page_render_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+    def test_home_page_uses_item_form(self):
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
 
 
 class ListViewTest(TestCase):
