bcf6dc43d2bc21f1755c54c38dd5e08bb11c51d3
Denis Knyazev <denis.m.knyazev@gmail.com>
2014-05-04 01:10:13 +0400

    Placeholder test_my_lists and move login checkers into base.

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -2,6 +2,7 @@ from .base import FunctionalTest
 import time
 from selenium.webdriver.support.ui import WebDriverWait
 
+TEST_EMAIL = "edith@mockmyid.com"
 
 class LoginTest(FunctionalTest):
 
@@ -12,21 +13,21 @@ class LoginTest(FunctionalTest):
         self.switch_to_new_window('Mozilla Persona')
 
         self.browser.find_element_by_id(
-            'authentication_email').send_keys("edith@mockmyid.com")
+            'authentication_email').send_keys(TEST_EMAIL)
         self.browser.find_element_by_tag_name('button').click()
 
         self.switch_to_new_window('To-Do')
 
-        self.wait_to_be_logged_in()
+        self.wait_to_be_logged_in(email=TEST_EMAIL)
 
         self.browser.refresh()
-        self.wait_to_be_logged_in()
+        self.wait_to_be_logged_in(email=TEST_EMAIL)
 
         self.browser.find_element_by_id('id_logout').click()
-        self.wait_to_be_logged_out()
+        self.wait_to_be_logged_out(email=TEST_EMAIL)
 
         self.browser.refresh()
-        self.wait_to_be_logged_out()
+        self.wait_to_be_logged_out(email=TEST_EMAIL)
 
         self.wait_for_element_with_id('id_logout')
         navbar = self.browser.find_element_by_css_selector('.navbar')
@@ -42,19 +43,3 @@ class LoginTest(FunctionalTest):
             retries -= 1
             time.sleep(0.5)
         self.fail('could not find window')
-
-    def wait_for_element_with_id(self, element_id):
-        WebDriverWait(self.browser, timeout=30).until(
-            lambda b: b.find_element_by_id(element_id),
-            'Could not find element with id {}. Page text was {}'.format(
-                element_id, self.browser.find_element_by_tag_name('body').text))
-
-    def wait_to_be_logged_in(self):
-        self.wait_for_element_with_id('id_logout')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn('edith@mockmyid.com', navbar.text)
-
-    def wait_to_be_logged_out(self):
-        self.wait_for_element_with_id('id_login')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertNotIn('edith@mockmyid.com', navbar.text)
