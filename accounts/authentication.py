669868bcdc7053d631afd2f5e816d41144840e8f
Denis Knyazev <denis.m.knyazev@gmail.com>
2014-05-05 06:42:22 +0400

    url, placeholder view, and first-cut templates for my_lists.

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -1,10 +1,13 @@
 import requests
+import logging
 from django.conf import settings
 from django.contrib.auth import get_user_model
 
 PERSONA_VERIFY_URL = 'https://verifier.login.persona.org/verify'
 
 User = get_user_model()
+
+logger = logging.getLogger(__name__)
 
 
 class PersonaAuthenticationBackend(object):
@@ -18,6 +21,8 @@ class PersonaAuthenticationBackend(object):
                 return User.objects.get(email=response.json()['email'])
             except User.DoesNotExist:
                 return User.objects.create(email=email)
+        else:
+            logger.warning('Persona says no. Json was {}'.format(response.json()))
 
     def get_user(self, email):
         try:
--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from selenium.webdriver.common.keys import Keys
 
 from .base import FunctionalTest
 from .server_tools import create_session_on_server
@@ -22,13 +23,26 @@ class MyListsTest(FunctionalTest):
         ))
 
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
-        email = 'edith@example.com'
-
-        self.browser.get(self.server_url)
-        self.wait_to_be_logged_out(email)
-
         # Edith is a logged-in user
-        self.create_pre_authenticated_session(email)
+        self.create_pre_authenticated_session('edith@example.com')
 
         self.browser.get(self.server_url)
-        self.wait_to_be_logged_in(email)
+        self.get_item_input_box().send_keys('Reticulate splines' + Keys.ENTER)
+        self.get_item_input_box().send_keys('Immanentize eschaton' + Keys.ENTER)
+        first_list_url = self.browser.current_url
+
+        self.browser.find_element_by_link_text('My lists').click()
+
+        self.browser.find_element_by_link_text('Reticulate splines').click()
+        self.assertEqual(self.browser.current_url, first_list_url)
+
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('Click cows' + Keys.ENTER)
+        second_list_url = self.browser.current_url
+
+        self.browser.find_element_by_link_text('My lists').click()
+        self.browser.find_element_by_link_text('Click cows').click()
+        self.assertEqual(self.browser.current_url, second_list_url)
+
+        self.browser.find_element_by_id('id_logout').click()
+        self.assertEqual(self.browser.find_elements_by_link_text('My lists'), [])
